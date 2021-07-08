08d1ab5b8023ea8e567ef9d8aee2b359ff96b7c1
Dudi Levy <dudi@elv8.pro>
2021-07-08 15:27:10 +0000

    update to running configuration

--- scripts/init-all.sh
+++ scripts/init-all.sh
@@ -3,6 +3,12 @@
 cd "$(dirname "$0")"
 cd ..
 
+. .env
+
+set -x #echo on
+
+docker network create ${GITLAB_NETWORK}
+
 mkdir ./vols/gitlab/config -p
 mkdir ./vols/gitlab/data -p
 mkdir ./vols/gitlab/logs -p
@@ -12,4 +18,7 @@ mkdir ./vols/sonarqube/data -p
 mkdir ./vols/sonarqube/extensions -p
 mkdir ./vols/sonarqube/logs -p
 mkdir ./vols/sonarqube/temp -p
-touch -a ./vols/gitlab-runner/config.toml
+mkdir ./vols/gitlab-runner/config -p
+touch -a ./vols/gitlab-runner/config/config.toml
+
+docker-compose up --build -d
--- scripts/runner_registration.sh
+++ scripts/runner_registration.sh
@@ -6,11 +6,14 @@ cd ..
 . .env
 
 docker-compose exec gitlab-runner gitlab-runner register --non-interactive \
-                                                         --url "http://gitlab/" \
-                                                         --registration-token "$GITLAB_RUNNER_TOKEN" \
+                                                         --url "http://gitlab" \
+                                                         --registration-token "${GITLAB_RUNNER_TOKEN}" \
                                                          --name "default-local" \
                                                          --executor "docker" \
-                                                         --docker-image alpine:latest \
+                                                         --docker-image ubuntu:latest \
                                                          --locked="false"  \
                                                          --run-untagged="true" \
-                                                         --access-level="not_protected"
+                                                         --access-level="not_protected" \
+                                                         --clone-url="http://gitlab" \
+                                                         --docker-volumes="/var/run/docker.sock:/var/run/docker.sock" \
+                                                         --docker-network-mode="${GITLAB_NETWORK}"

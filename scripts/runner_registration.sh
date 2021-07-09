9985f59b42d52c3731d7c020019b7c8365e806a9
Dudi Levy <dudi@elv8.pro>
2021-07-09 13:19:25 +0000

    update runner to be configured from docker-compose

--- scripts/runner_registration.sh
+++ scripts/runner_registration.sh
@@ -5,15 +5,4 @@ cd ..
 
 . .env
 
-docker-compose exec gitlab-runner gitlab-runner register --non-interactive \
-                                                         --url "http://gitlab" \
-                                                         --registration-token "${GITLAB_RUNNER_TOKEN}" \
-                                                         --name "default-local" \
-                                                         --executor "docker" \
-                                                         --docker-image ubuntu:latest \
-                                                         --locked="false"  \
-                                                         --run-untagged="true" \
-                                                         --access-level="not_protected" \
-                                                         --clone-url="http://gitlab" \
-                                                         --docker-volumes="/var/run/docker.sock:/var/run/docker.sock" \
-                                                         --docker-network-mode="${GITLAB_NETWORK}"
+docker-compose exec gitlab-runner gitlab-runner register --non-interactive
